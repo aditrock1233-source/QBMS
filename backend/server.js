56ddc4b53fe575245f73bf5cb349c0abc3dd64ff
Aditya Agrawal <adit.rock1233@gmail.com>
2026-07-02 11:15:36 +0530

    Updated CORS with stable URL

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://qbms-1ad4uz0nu-question-bank-management-system.vercel.app'],
+  origin: [
+  'http://localhost:3000',
+  'https://qbms-iota.vercel.app'
+],
   exposedHeaders: ['Content-Disposition']
 }));
 app.use(express.json());
